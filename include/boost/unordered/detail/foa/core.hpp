37ef1611b2abbc3cd757cd0790f58210dc3e8cbf
joaquintides <joaquin.lopezmunoz@gmail.com>
2025-08-23 12:30:03 +0200

    changed max load factor to 0.8 to mach indivi

--- include/boost/unordered/detail/foa/core.hpp
+++ include/boost/unordered/detail/foa/core.hpp
@@ -1255,7 +1255,7 @@ _STL_RESTORE_DEPRECATED_WARNING
  * needing to pull it from an instantiated class template such as the table
  * class.
  */
-static constexpr float mlf=0.875f;
+static constexpr float mlf=0.8f;
 
 template<typename Group,typename Element>
 struct table_locator
